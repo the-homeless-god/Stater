28c92b1c022baef83cee93840ce09ea4220f6e6d
Zimtir <zimtir@mail.ru>
2020-04-05 04:42:11 +0300

    drop comm

--- src/tools/collect.tool.ts
+++ src/tools/collect.tool.ts
@@ -17,44 +17,44 @@ export default class CollectTool {
   }
 
   init = async () => {
-    // cron.schedule('*/15 * * * *', async () => {
-    //   let stats: IStat[] = await this.statRepository.getAll()
+    cron.schedule('*/15 * * * *', async () => {
+      let stats: IStat[] = await this.statRepository.getAll()
 
-    //   const browser = await puppeteer.launch({
-    //     headless: true,
-    //     args: ['--no-sandbox', '--mute-audio', '--disable-gpu'],
-    //   })
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--mute-audio', '--disable-gpu'],
+      })
 
-    //   const page = await browser.newPage()
+      const page = await browser.newPage()
 
-    //   await page.goto(
-    //     'https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory',
-    //     { waitUntil: 'load' },
-    //   )
+      await page.goto(
+        'https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory',
+        { waitUntil: 'load' },
+      )
 
-    //   let actStats = await page.evaluate(parser)
+      let actStats = await page.evaluate(parser)
 
-    //   Logger.log(`parsed ${actStats.length} statistics`)
-    //   await browser.close()
+      Logger.log(`parsed ${actStats.length} statistics`)
+      await browser.close()
 
-    //   if (actStats && actStats.length > 0) {
-    //     actStats.map(async act => {
-    //       let existData = stats.find(stat => stat.country === act.country)
-    //       if (existData) {
-    //         await this.statRepository.update({
-    //           id: existData.id,
-    //           country: existData.country,
-    //           icon: act.icon,
-    //           case: act.case,
-    //           death: act.death,
-    //           recov: act.recov,
-    //           time: new Date(),
-    //         })
-    //       } else {
-    //         await this.statRepository.add(act)
-    //       }
-    //     })
-    //   }
-    // })
+      if (actStats && actStats.length > 0) {
+        actStats.map(async act => {
+          let existData = stats.find(stat => stat.country === act.country)
+          if (existData) {
+            await this.statRepository.update({
+              id: existData.id,
+              country: existData.country,
+              icon: act.icon,
+              case: act.case,
+              death: act.death,
+              recov: act.recov,
+              time: new Date(),
+            })
+          } else {
+            await this.statRepository.add(act)
+          }
+        })
+      }
+    })
   }
 }
