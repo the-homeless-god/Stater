cff8e2f5d056ce1b17c42c69fcc5b62c4f7c0f66
Zimtir <zimtir@mail.ru>
2020-04-06 01:04:36 +0300

    add pagination

--- src/components/stat/List.svelte
+++ src/components/stat/List.svelte
@@ -2,6 +2,7 @@
   import Box from '../common/Box.svelte'
 
   import { stats } from '../../stores/store.ts'
+  import ItemList from 'svelte-item-list/dist/index.js'
 </script>
 
 <style>
@@ -28,8 +29,8 @@
   }
 
   .item-list {
-    margin: 20px;
-    padding: 20px;
+    margin: 0px 20px 0px 20px;
+    padding: 0px 20px 0px 20px;
     max-width: fit-content;
     height: fit-content;
     width: 100%;
@@ -43,17 +44,31 @@
 {#if $stats.length > 0}
   <div class="full-width item-list">
 
-    {#each $stats as stat}
-      <Box classList={'list-stat-block'}>
-        <div class="full-width flex-center contry">
-          <img src={stat.icon} alt="country icon" />
-          <b>{stat.country}</b>
-        </div>
-        <div class="full-width flex-center">
-          {@html stat.desc}
-        </div>
-      </Box>
-    {/each}
+    <ItemList
+      needPag={true}
+      pageSize={9}
+      needIndex={false}
+      needPoint={false}
+      needBody={false}
+      endpointIsStore={true}
+      endpoint={stats}
+      let:items>
+      <div slot="loading">loading...</div>
+
+      <div class="full-flex full-width" slot="body">
+        {#each items as item}
+          <Box classList={'list-stat-block'}>
+            <div class="full-width flex-center contry">
+              <img src={item.icon} alt="country icon" />
+              <b>{item.country}</b>
+            </div>
+            <div class="full-width flex-center">
+              {@html item.desc}
+            </div>
+          </Box>
+        {/each}
+      </div>
+    </ItemList>
 
   </div>
 {/if}
