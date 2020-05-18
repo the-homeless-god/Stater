443b574cb43be6382c3f3f014104fb354c156325
Zimtir <zimtir@mail.ru>
2020-05-18 23:42:55 +0300

    add update command

--- bot/sources/bots/vk.bot.ts
+++ bot/sources/bots/vk.bot.ts
@@ -8,6 +8,10 @@ export default class VKBot {
       text: string
       command: string
     }
+    update: {
+      text: string
+      command: string
+    }
   }
 
   constructor(token: string) {
@@ -21,17 +25,28 @@ export default class VKBot {
       `,
         command: '/start',
       },
+      update: {
+        text: `
+        Добро пожаловать!
+        Здесь Вы можете узнать последнюю информацию о статистике по COVID-19
+        Вся информация берётся из https://corona.notify.wtf
+      `,
+        command: '/start',
+      },
     }
 
     this.bot = new VKBotAPI(token)
-
+    this.startHandler()
     this.bot.startPolling()
   }
 
   startHandler = () => {
     this.bot.command(this.configuration.start.command, (ctx: any) => {
-      console.log(ctx)
       ctx.reply(this.configuration.start.text)
+    })
+
+    this.bot.command(this.configuration.update.command, (ctx: any) => {
+      ctx.reply(this.configuration.update.text)
     })
   }
 }
