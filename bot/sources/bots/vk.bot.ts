837f16f7494867c47c7cd42b11a2a8e27284b942
Zimtir <zimtir@mail.ru>
2020-05-19 01:05:56 +0300

    add vk bot

--- bot/sources/bots/vk.bot.ts
+++ bot/sources/bots/vk.bot.ts
@@ -1,52 +1,106 @@
+import IButton from '../interfaces/button.interface'
+import { getSocialButtons } from '../helpers/button.helper'
+import SocialEnum from '../enums/social.enum'
+import ButtonEnum from '../enums/button.enum'
+import {
+  getCountries,
+  calcByProp,
+  getTopTenByProp,
+} from '../helpers/api.helper'
+
 const VKBotAPI = require('node-vk-bot-api')
+const VKMarkup = require('node-vk-bot-api/lib/markup')
 
 export default class VKBot {
   token: string
   bot: any
-  configuration: {
-    start: {
-      text: string
-      command: string
-    }
-    update: {
-      text: string
-      command: string
-    }
-  }
+
+  buttons: IButton[]
 
   constructor(token: string) {
     this.token = token
-    this.configuration = {
-      start: {
-        text: `
-        Добро пожаловать!
-        Здесь Вы можете узнать последнюю информацию о статистике по COVID-19
-        Вся информация берётся из https://corona.notify.wtf
-      `,
-        command: '/start',
-      },
-      update: {
-        text: `
-        Добро пожаловать!
-        Здесь Вы можете узнать последнюю информацию о статистике по COVID-19
-        Вся информация берётся из https://corona.notify.wtf
-      `,
-        command: '/start',
-      },
-    }
+
+    this.buttons = getSocialButtons(SocialEnum.VK)
 
     this.bot = new VKBotAPI(token)
     this.startHandler()
+    this.menuHandler()
+    this.buttonHandler()
     this.bot.startPolling()
   }
 
-  startHandler = () => {
-    this.bot.command(this.configuration.start.command, (ctx: any) => {
-      ctx.reply(this.configuration.start.text)
-    })
+  menuHandler = () => {
+    let commandList = ['/update', '/menu']
 
-    this.bot.command(this.configuration.update.command, (ctx: any) => {
-      ctx.reply(this.configuration.update.text)
+    commandList.forEach((command) => {
+      this.bot.command(command, (ctx: any) => {
+        ctx.reply('Выберите пункт меню', null, this.getButtonPreview())
+      })
+    })
+  }
+
+  getButtonPreview = () => {
+    return VKMarkup.keyboard([
+      [this.buttons[0]],
+      [this.buttons[1]],
+      [this.buttons[2]],
+      [this.buttons[3]],
+      [this.buttons[4]],
+      [this.buttons[5]],
+    ])
+  }
+
+  buttonHandler = () => {
+    this.buttons.forEach((button) => {
+      //@ts-ignore
+      this.bot.command(button.action.label, async (ctx: any) => {
+        try {
+          //@ts-ignore
+          const command = JSON.parse(button.action.payload)
+          if (command) {
+            let countries = []
+            let response = null
+            let output = ''
+
+            switch (command.operation) {
+              case ButtonEnum.caseCount:
+              case ButtonEnum.deathCount:
+              case ButtonEnum.recoveryCount:
+                countries = await getCountries()
+                response = calcByProp(countries, command.property)
+                output = `Всего ${command.prefix} в мире: ${response}`
+
+                ctx.reply(output)
+
+                break
+              case ButtonEnum.topTenByCase:
+              case ButtonEnum.topTenByDeath:
+              case ButtonEnum.topTenByRecovery:
+                countries = await getCountries()
+                response = getTopTenByProp(countries, command.property)
+                output = response.join(', ')
+
+                ctx.reply(output)
+                break
+              default:
+                break
+            }
+          }
+        } catch (ex) {
+          console.log(ex)
+        }
+      })
+    })
+  }
+
+  startHandler = () => {
+    this.bot.command('/start', (ctx: any) => {
+      ctx.reply(`
+        Добро пожаловать!
+        Здесь Вы можете узнать последнюю информацию о статистике по COVID-19
+        Вся информация берётся из https://corona.notify.wtf
+        Напишите "/update" или "/menu" чтобы получить меню
+      `)
     })
   }
 }
